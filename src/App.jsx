a6eab50ccc326fd41501967b7cfd22da9c637b78
KarlinCoder <giancarlo.frontend@gmail.com>
2024-10-09 19:31:51 -0400

    Ultimo commit asxddx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Game from "./components/Game";
 
 export default function App() {
   const [showGame, setShowGame] = useState(false);
-
+  // Solucion de altura
   return (
-    <div className="relative flex flex-col md:flex-row md:gap-10 justify-center items-center bg-neutral-900 h-screen w-screen ">
+    <div className="relative flex flex-col md:flex-row md:gap-10 justify-center items-center bg-neutral-900 h-dvh w-screen lg:h-screen lg:w-screen">
       <Menu showGame={showGame} setShowGame={setShowGame} />
       <Game showGame={showGame} setShowGame={setShowGame} />
     </div>
